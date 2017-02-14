4bbd3dec8ee313059eeab86f2767484ab3d973c1
h4s33b <haseeb.rehman@iunc.edu.pk>
2017-02-14 03:54:21 -0800

    Implemented Menu Component

---
+++ menu-sample/src/menu/menu.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Link from '../link/link';
+
+class Menu extends Component {
+    render() {
+        let menus = ['Home',
+            'About',
+            'Services',
+            'Portfolio',
+            'Contact us'
+            ]
+        return (
+            <div>
+                {
+                    menus.map((v, i) => {
+                        return <div key={i}><Link label={v}/></div>
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default Menu;
