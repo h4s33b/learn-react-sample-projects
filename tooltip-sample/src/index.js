e2ae358d8adaf14f5313241520753d2111911b86
h4s33b <haseeb.rehman@iunc.edu.pk>
2017-02-14 06:02:51 -0800

    Implemented tooltip-sample

---
+++ tooltip-sample/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tooltip from './tooltip/tooltip';
+import './index.css';
+
+ReactDOM.render(
+  <div><Tooltip text="The book you're reading now">React Quickly</Tooltip> was published in 2017. Its <Tooltip text="World is Saying">awesome!</Tooltip></div>,
+  document.getElementById('root')
+);
